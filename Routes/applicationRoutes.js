11ce79de760d6e9b9e66694760ae15fa882aee88
purva jage <purvajage44@gmail.com>
2025-07-13 12:35:39 +0530

    commit

--- Routes/applicationRoutes.js
+++ Routes/applicationRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 const applicationController = require('../controller/applicationController');
-
+// This file handles routes for applications, including retrieving, creating, updating, and deleting applications
 
 router.get("/", applicationController.getApplications); 
 router.post("/", applicationController.createApplication); 
--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { register, login } = require('../controller/authController'); 
 const router = express.Router();
-
+// This file handles user registration and login functionalities
 router.post('/register', register); 
 router.post('/login', login);       
 
