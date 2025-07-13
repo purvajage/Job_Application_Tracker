741cbd6a700b0543ee14874f8130b1a1db852a34
purva jage <purvajage44@gmail.com>
2025-07-13 12:36:20 +0530

    commit

--- Routes/dashboardRoutes.js
+++ Routes/dashboardRoutes.js
@@ -4,7 +4,7 @@ const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
-
+// This file handles routes for the dashboard, including retrieving dashboard data
 router.use(authMiddleware);
 
 router.get('/', getDashboard); 
--- Routes/notificationRouter.js
+++ Routes/notificationRouter.js
@@ -5,7 +5,7 @@ const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.use(authMiddleware);
-
+// This file handles routes for notifications, including retrieving and marking notifications as read
 router.get('/', getNotifications); 
 router.put('/:id/read', markAsRead); 
 
