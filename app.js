1ab8fbb7993e810a86f0351ff20c1e10d6e78b92
purva jage <purvajage44@gmail.com>
2025-07-14 20:41:50 +0530

    commit

--- app.js
+++ app.js
@@ -1,27 +1,18 @@
-
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
 const applicationRoutes = require('./routes/applicationRoutes');
 const authRoutes = require('./routes/authRoutes');
 const notificationRoutes = require('./routes/notificationRouter');
 const dashboardRoutes = require('./routes/dashboardRoutes');
-
-
 dotenv.config();
-
-
 const app = express();
 app.use(express.json());
-
 app.use(cors()); 
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true })); 
-
-
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected successfully'))
@@ -29,10 +20,8 @@ mongoose
     console.error('MongoDB connection failed:', err);
     process.exit(1); 
   });
-
-
 app.get('/', (req, res) => {
-  res.send('Server is running!');
+  res.send('Server is properly running!');
 });
 
 
@@ -44,5 +33,5 @@ app.use('/api/dashboard', dashboardRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on Port :  ${PORT}`);
 });
