6ed8661c171c3b464f2951abb991c7deab78d1bb
purva jage <purvajage44@gmail.com>
2025-07-16 10:33:14 +0530

    COMMIT

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const User = require('../model/userModel'); 
-
+// Middleware to authenticate user based on JWT token
 const authMiddleware = async (req, res, next) => {
     try {
         
@@ -8,13 +8,13 @@ const authMiddleware = async (req, res, next) => {
         if (!authHeader || !authHeader.startsWith("Bearer ")) {
             return res.status(401).json({ error: "Unauthorized: No token provided" });
         }
-
+// Check if the token is present in the Authorization header
         const token = authHeader.split(" ")[1];
         if (!token) {
             return res.status(401).json({ error: "Unauthorized: Token missing" });
         }
 
-       
+       // Verify the token
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         if (!decoded) {
             return res.status(401).json({ error: "Unauthorized: Invalid token" });
